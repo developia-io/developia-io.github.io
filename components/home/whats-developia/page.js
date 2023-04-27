5949c890452126b47f15fa57f97ed65a5b72ae1c
Oruchan Asar <oruchan.asar@gmail.com>
2023-04-27 14:39:25 +0300

    responsive design for whats-developia

--- components/home/whats-developia/page.js
+++ components/home/whats-developia/page.js
@@ -6,41 +6,43 @@ import toggleOn from "@/public/home/toggle-on.png";
 
 export default function WhatsDevelopia() {
   return (
-    <div className="container">
-      <div className={styles.head}>
-        <h1 className="title">What’s Developia?</h1>
-        <p className={styles.description}>
-          Developia supports entrepreneurship ecosystem with its design and
-          advanced development capabilities. Positioning as a Venture Studio,
-          Developia offers different partnership models to assist companies in
-          cashflow management.
-        </p>
-      </div>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.head}>
+          <h1 className="title">What’s Developia?</h1>
+          <p className={styles.description}>
+            Developia supports entrepreneurship ecosystem with its design and
+            advanced development capabilities. Positioning as a Venture Studio,
+            Developia offers different partnership models to assist companies in
+            cashflow management.
+          </p>
+        </div>
 
-      <div className={styles.container}>
-        <div className={styles.card}>
-          <Image src={paintBrush} alt="paint brush" />
-          <h2>New Design</h2>
-          <p>
-            Carefully crafted precise design, with harmonious typography and
-            perfect padding around every component
-          </p>
-        </div>
-        <div className={styles.card}>
-          <Image src={code} alt="code" />
-          <h2>New Code</h2>
-          <p>
-            Built with HTML, CSS and JS. Startup’s code is easy to under-stand
-            and easy to change. No other frameworks needed.
-          </p>
-        </div>
-        <div className={styles.card}>
-          <Image src={toggleOn} alt="toggle on" />
-          <h2>New Solutions</h2>
-          <p>
-            Transform your ideas into reality and launch a beautiful site with
-            minimal frustration.
-          </p>
+        <div className={styles.card_container}>
+          <div className={styles.card}>
+            <Image src={paintBrush} alt="paint brush" />
+            <h2>New Design</h2>
+            <p>
+              Carefully crafted precise design, with harmonious typography and
+              perfect padding around every component
+            </p>
+          </div>
+          <div className={styles.card}>
+            <Image src={code} alt="code" />
+            <h2>New Code</h2>
+            <p>
+              Built with HTML, CSS and JS. Startup’s code is easy to under-stand
+              and easy to change. No other frameworks needed.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <Image src={toggleOn} alt="toggle on" />
+            <h2>New Solutions</h2>
+            <p>
+              Transform your ideas into reality and launch a beautiful site with
+              minimal frustration.
+            </p>
+          </div>
         </div>
       </div>
     </div>
