1b07a43fa96a0eba11b2d4dcee2dd62934282021
Oruchan Asar <oruchan.asar@gmail.com>
2023-04-27 12:03:05 +0300

    add responsive design for header

--- components/header/page.js
+++ components/header/page.js
@@ -1,28 +1,79 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import styles from "./header.module.css";
 import developia from "@/public/DEVELOPIA.png";
+import hamburger from "./hamburger.svg";
+import cross from "./cross.svg";
 import Button from "../button/page";
+import { useState } from "react";
 
-export default function Header() {
+function Header() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    const menu = document.getElementById("menu");
+    const hamburger = document.getElementById("hamburger");
+    const cross = document.getElementById("cross");
+    isOpen
+      ? ((menu.style.display = "flex"),
+        (hamburger.style.display = "none"),
+        (cross.style.display = "flex"))
+      : ((menu.style.display = "none"),
+        (cross.style.display = "none"),
+        (hamburger.style.display = "flex"));
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
-        <Link href="/">
-          <Image src={developia} alt="developia" priority />
-        </Link>
+        <div className={styles.container}>
+          <Link className={styles.developia} href="/">
+            <Image src={developia} alt="developia" priority />
+          </Link>
+          <Image
+            id="hamburger"
+            className={styles.hamburger}
+            src={hamburger}
+            alt="hamburger"
+            width={30}
+            onClick={() => toggle()}
+          />
+          <Image
+            id="cross"
+            className={styles.cross}
+            src={cross}
+            alt="cross"
+            width={30}
+            onClick={() => toggle()}
+          />
+        </div>
 
-        <div className={styles.menu}>
+        <div className={styles.menu} id="menu">
           <div className={styles.link}>
-            <Link href="#services">Services</Link>
-            <Link href="#technologies">Technologies</Link>
-            <Link href="#works">Works</Link>
-            <Link href="/about-us">About Us</Link>
+            <Link href="#services" onClick={() => toggle()}>
+              Services
+            </Link>
+            <Link href="#technologies" onClick={() => toggle()}>
+              Technologies
+            </Link>
+            <Link href="#works" onClick={() => toggle()}>
+              Works
+            </Link>
+            <Link href="/about-us" onClick={() => toggle()}>
+              About Us
+            </Link>
           </div>
-          <Button label="Get in Touch" navigateTo="get-in-touch" />
+          <div onClick={() => toggle()}>
+            <Button label="Get in Touch" navigateTo="get-in-touch" />
+          </div>
         </div>
       </div>
     </header>
   );
 }
+
+export default Header;
