3ca26e0a550dad517fcc3bdf7b755140a4199626
Oruchan Asar <oruchan.asar@gmail.com>
2023-05-08 12:59:39 +0300

    add parallax effect to the works section

--- components/home/our-works/card/page.js
+++ components/home/our-works/card/page.js
@@ -1,39 +1,42 @@
 import Image from "next/image";
+import Tilty from "react-tilty";
 import styles from "./card.module.css";
 
 export default function Card({ cardInfo }) {
   return (
-    <div
-      className={styles.wrapper}
-      style={{
-        background: cardInfo.bgColor,
-      }}
-    >
-      <div className={styles.logo}>
-        <Image
-          src={cardInfo.logo}
-          alt={cardInfo.title}
-          width={200}
-          height={70}
-        />
-      </div>
-
-      <div className={styles.body} style={{ color: cardInfo.color }}>
-        <h1>{cardInfo.title}</h1>
-        <p>{cardInfo.description}</p>
-      </div>
-
-      {cardInfo?.bodyImage ? (
-        <div className={styles.body_image}>
+    <Tilty max={20} scale={0.95} glare={true} maxGlare={0.3}>
+      <div
+        className={styles.wrapper}
+        style={{
+          background: cardInfo.bgColor,
+        }}
+      >
+        <div className={styles.logo}>
           <Image
-            src={cardInfo.bodyImage}
-            alt={cardInfo.description}
-            width={333.33}
+            src={cardInfo.logo}
+            alt={cardInfo.title}
+            width={200}
+            height={70}
           />
         </div>
-      ) : (
-        ""
-      )}
-    </div>
+
+        <div className={styles.body} style={{ color: cardInfo.color }}>
+          <h1>{cardInfo.title}</h1>
+          <p>{cardInfo.description}</p>
+        </div>
+
+        {cardInfo?.bodyImage ? (
+          <div className={styles.body_image}>
+            <Image
+              src={cardInfo.bodyImage}
+              alt={cardInfo.description}
+              width={333.33}
+            />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+    </Tilty>
   );
 }
